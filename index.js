7ccdfe92c7e1c15314a66905a517a86fba3c8c5a
PLAYMAN11 <EmmanuelGZHZ@hotmail.com>
2024-07-03 12:23:56 -0600

    Creacion de servidor y conexion a la base de datos

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const run = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "abarrotestid31m",
+    port: 3306,
+});
+
 const server = express();
 
-server.get("/", (req , res) => {
-    console.log("GET /");
-    console.log(req.query);
-    res.send("Hello world");
+server.use(bodyParser.json());
 
+server.get("/", (req, res) => {
+    console.log("GET /");
+    res.send("hola mundo");
 });
 
 server.get("/user", (req, res) => {
-    res.send("useraesi2145")
-})
-server.listen(3000, () =>{
-    console.log("Server is running")
+    run.query('SELECT * FROM alumnos', (err, resultados) => {
+        if (err) {
+            console.log("No se pudo acceder a la tabla", err);
+        } else {
+            console.log("Se accedió a la tabla");
+            console.log(resultados);
+            res.json(resultados);
+        }
+    });
 });
+
+server.post("/user", (req, result) => {
+    const { NNombre, GGrado } = req.body;
+    run.query("INSERT INTO alumnos (Nombre, grado) VALUES (?, ?)", [NNombre, GGrado], (err, result) => {
+        if (err) {
+            console.log("Error al insertar en la tabla", err);
+        } else {
+            console.log("Datos insertados correctamente");
+        }
+    });
+});
+
+server.listen(
+     () => {
+    console.log("Server is running on port 3000");
+});
+
